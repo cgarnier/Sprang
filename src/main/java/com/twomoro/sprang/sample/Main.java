45b10e802bb79e2a924a914919ad615cb67017c0
Clément Garnier <cle.garnier@gmail.com>
2014-05-06 17:26:03 +0200

    Add files

--- src/main/java/com/twomoro/sprang/sample/Main.java
+++ src/main/java/com/twomoro/sprang/sample/Main.java
@@ -6,8 +6,9 @@ import com.github.mustachejava.MustacheFactory;
 import com.twomoro.sprang.generator.ResourceGenerator;
 import com.twomoro.sprang.models.ControllerDetails;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 /**
@@ -21,10 +22,25 @@ public class Main {
     }
 
     public Main(String[] paths) {
+        String dir = "D:\\DEV\\AirCobot\\client\\app\\SprangOutput\\";
+        String configName = "bflyApi";
+        File file = new File(dir + "test.txt");
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        PrintStream stream = new PrintStream(out);
+
+        stream.println("test1");
+        stream.println("test2");
+
         controllers = new ArrayList<ControllerDetails>();
         System.out.println("Starting sample app.");
         ResourceGenerator gen = new ResourceGenerator();
-        gen.setConfigName("bflyApi");
+        gen.setConfigName(configName);
 
         this.controllers = gen.fromJars(paths);
 /*        for(ControllerDetails cd :this.controllers){
@@ -34,8 +50,18 @@ public class Main {
 
         MustacheFactory mf = new DefaultMustacheFactory();
         Mustache mustache = mf.compile("templates/serviceTpl.mustache");
+
+        for (ControllerDetails c : this.controllers) {
+            try {
+                mustache.execute(new PrintWriter(dir + c.getServiceName() +".js"), c).flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        mustache = mf.compile("templates/allServiceTpl.mustache");
         try {
-            mustache.execute(new PrintWriter(System.out), this).flush();
+            mustache.execute(new PrintWriter(dir + configName + "Services.js"), this).flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
